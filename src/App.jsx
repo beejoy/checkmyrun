28b8b64d9ff80f84c600ed96fd2d4712429a367d
Bijay Rai <bijay.chamling@gmail.com>
2024-07-29 18:51:15 +0545

    Adjusted records for 10536 and 15575

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,16 @@ const App = () => {
           )
         )
       : setRunner(runData.filter((data) => data.ID === Number(searchTerm)));
+
+    searchTerm.match(/parent:[a-z]/)
+      ? setRunner(
+          runData.filter((data) =>
+            data.Parent.toLowerCase().match(
+              searchTerm.length > 0 ? searchTerm : "xiaomi"
+            )
+          )
+        )
+      : setRunner(runData.filter((data) => data.ID === Number(searchTerm)));
     setSearchTerm("");
   };
 
